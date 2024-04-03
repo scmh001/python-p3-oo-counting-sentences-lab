4d96736933e85046363918d6d69bd045a6ba2912
Shukri Hussein <husseinshukri@outlook.com>
2024-04-03 11:35:59 -0400

    lab-complete

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,39 @@
 #!/usr/bin/env python3
-
+import re
 class MyString:
-  pass
+  def __init__(self, value=""):
+    self.value = value
+    
+  @property
+  def value(self):
+    return self._value
+  
+  @value.setter
+  def value(self, stringVal):
+     if isinstance(stringVal, str):
+      self._value = stringVal
+     else:
+         print("The value must be a string.")
+         
+         
+  sentences = ["This, well, is a sentence. This is too!! And so is this, I think? Woo..."]
+         
+  def is_sentence(self):
+    return self._value.endswith(".")
+  
+  def is_question(self):
+    return self._value.endswith("?")
+  
+  def is_exclamation(self):
+    return self._value.endswith("!")
+  
+  def count_sentences(self):
+    sentences = self._value.replace('!', ".").replace('?','.').split('.')
+    return sum(1 for sentence in sentences if sentence.strip())
+    
+    
+    # pattern = r'[.?!]+'
+    # matches = re.findall(pattern, self.value)
+    # return len(matches)
+    
+print(sum())
